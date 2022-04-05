1163d806a1ad6cea2dcaf1a2ecf4929300cb5895
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-04 23:29:20 -0300

    Criando a função começar etapa

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = false;
+let votos = [];
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
+
+    for(let i=0;i<etapa.numeros;i++) {
+        if(i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}

c11dc9a27fc43f7ec59cac400183be82545f3e5c
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-09 21:39:47 -0300

    Configurando as etapas

---
+++ etapas.js
@@ -0,0 +1,23 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '29030',
+                nome: 'Guilherme Mendes',
+                partido: 'JLJM',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '24070',
+                nome: 'Alice Mendes',
+                partido: 'JLJM',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
